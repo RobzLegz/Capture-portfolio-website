78e7670166e54512a9314656247bca22bbdb1362
Robzlegz <robzlegz@gmail.com>
2020-12-30 17:52:37 +0200

    start work transitions

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -5,16 +5,24 @@ import goodtimes from "../img/goodtimes-small.png";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import {motion} from "framer-motion";
-import {pageAnimation} from "../animation";
+import {pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer} from "../animation";
 
 function OurWork() {
     return (
         <Work variants={pageAnimation} initial="hidden" animate="show" exit="exit" style={{background:"fff"}}>
+            <motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
             <Movie>
-                <h2>The Athlete</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to="/work/the-athlete">
-                    <img src={athlete} alt="athlete"/>
+                    <Hide>
+                        <motion.img variants={photoAnim} src={athlete} alt="athlete"/>
+                    </Hide>
                 </Link>
             </Movie>
             <Movie>
@@ -39,6 +47,7 @@ const Work = styled(motion.div)`
     min-height:100vh;
     overflow:hidden;
     padding:5rem 10rem;
+    background: #fff;
     h2{
         padding: 1rem 0rem;
     }
@@ -48,7 +57,7 @@ const Movie = styled.div`
     padding-bottom:10rem;
     .line{
         height:0.5rem;
-        background:#cccccc;
+        background:#23d997;
         margin-bottom:3rem;
     }
     img{
@@ -56,6 +65,32 @@ const Movie = styled.div`
         height:70vh;
         object-fit:cover;
     }
+`;
+
+const Hide = styled.div`
+    overflow:hidden;
+`;
+
+const Frame1 = styled(motion.div)`
+    position: fixed;
+    left:0;
+    top: 10%;
+    width:100%;
+    height:100vh;
+    background: #fffebf;
+    z-index:2;
+`;
+
+const Frame2 = styled(Frame1)`
+    background: #ff8eff;
+`;
+
+const Frame3 = styled(Frame1)`
+    background: #8ed2ff;
+`;
+
+const Frame4 = styled(Frame1)`
+    background: #8effa0;
 `;
 
 export default OurWork;
