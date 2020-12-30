334d821c1a54debb702f3e42474eb17950fcc94d
Robzlegz <robzlegz@gmail.com>
2020-12-30 23:54:14 +0200

    faq animations

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,43 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { About } from "../styles";
 import styled from "styled-components";
+import Toggle from "./Toggle";
+import {AnimateSharedLayout} from "framer-motion";
 
 function FaqSection() {
     return (
         <Faq className="faq">
             <h2>Any Questions?<span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different payment methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <AnimateSharedLayout>
+                <Toggle title="How Do I Start?">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
+                        </div>
+                </Toggle>
+                <Toggle title="Daily Schedule">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
+                        </div>
+                </Toggle>
+                <Toggle title="Different payment methods">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
+                        </div>
+                </Toggle>
+                <Toggle title="What products do you offer?">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.</p>
+                        </div>
+                </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     );
 }
