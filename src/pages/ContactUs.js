b4080a21d38da45a0d80a356df653574531bf3e4
Robzlegz <robzlegz@gmail.com>
2020-12-30 10:14:39 +0200

    page transitions

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {motion} from "framer-motion";
+import {pageAnimation} from "../animation";
 
 function ContactUs() {
     return (
-        <div>
+        <motion.div variants={pageAnimation} initial="hidden" animate="show" exit="exit" style={{background:"fff"}}>
             <h1>Contact us</h1>
-        </div>
+        </motion.div>
     );
 };
 
